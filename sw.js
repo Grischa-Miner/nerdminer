5febfcc6c338fdddc780c0bda9034089df8d4da3
Grischa-Miner <durisch@gmail.com>
2026-05-29 00:50:05 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'nerdminer-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // Always fetch API calls live
+  if (url.hostname === 'blitzpool.yourdevice.ch') {
+    e.respondWith(fetch(e.request).catch(() => new Response('{}', { headers: { 'Content-Type': 'application/json' } })));
+    return;
+  }
+  // Cache-first for app shell
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
